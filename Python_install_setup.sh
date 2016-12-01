416b6a90f389d92c43e919c559cd6b1c147a55fe
vndk36 <victor.faraut@gmail.com>
2016-12-01 08:58:53 +0000

    Merging Readme (#2)
    * Modified readme file
    * Quick setup of python mudules for Raspberry Pi

---
+++ Python_install_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Setup a new raspberry pi jessie (not lite) for using the KNX, Amazon Echo library
+# 
+
+#sudo apt-get update
+#sudo apt-get upgrade
+
+sudo apt-get install python3-picamera
+sudo apt-get install python3-pip
+
+sudo su
+# as sudo user:
+
+pip install -U pip setuptools
+pip3 install flask-ask
+pip3 install knxip
+
+exit
+

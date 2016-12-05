601d3f79486d68d6494c48603469115702f37ba0
Faraut Victor, ENT-DES-DTO <Viktor.Faraut@swisscom.com>
2016-12-05 18:51:57 +0100

    Checking for light before switching them, and then restore them.
    BUG: get the rgb info doesn't work properly and return value just for the blue
    TODO: fix the "get" function

--- knx_demo.py
+++ knx_demo.py
@@ -13,121 +13,123 @@ import src.constants as constants
 def test_only_one_value(value=None):
     if not value:
         return 0
-    if len(value.split(' '))==1:
+    if len(value.split(' ')) == 1:
         try:
-            int(value)
-            if value< 0 & value>255:
-                return 0
-            else:
-                return 1
+            value = int(value)
         except ValueError:
             return 0
     else:
         return 0
-    
-    
+
+
 def help_text():
     return "disco, all_on"
 
-def disco(tunnel, value=None):
 
+def disco(tunnel, value=None):
     if not value:
         while True:
-            value = input("How many time would you like to run "+sys._getframe().f_code.co_name+" ? (put zero for timeless)\n")
+            value = input(
+                "How many time would you like to run " + sys._getframe().f_code.co_name + " ? (put zero for timeless)\n")
             if test_only_one_value(value):
                 break
             else:
-                print("The value must be on number between 0-255")
-            
-    #continue to ask while you don't enter something correct
+                print("The value must be on number between")
 
-    #sdl_knx.disco_RGB_mode(tunnel)
-    return "Disco for "+str(value)
+    # continue to ask while you don't enter something correct
+    print("Disco for " + str(value))
+    sdl_knx.save_rgb_all(tunnel)
+    for i in range(0, int(int(value)/3)):
+        sdl_knx.set_all_rgb_random(tunnel)
+        time.sleep(3)
+
+    sdl_knx.restore_rgb_all(tunnel)
+    return True
+
 
 def all_on(tunnel, value=None):
-
     if not value:
         while not value:
             value = input("How many time would you like to run all on ? (put zero for timeless)\n")
-        
-    #sdl_knx.set_all_rgb(tunnel, [0,0,0,200]
-    return "All_on for "+str(value)
+
+    sdl_knx.set_all_rgb(tunnel, [0,0,0,200])
+    return "All_on for " + str(value)
 
 
 def multiple_choice(choice_str, tunnel, value=None):
-    if choice_str== 'help':
-        return_str= help_text()
+    if choice_str == 'help':
+        return_str = help_text()
 
-    elif choice_str== 'disco':
-        return_str= (disco(tunnel, value))
+    elif choice_str == 'disco':
+        return_str = (disco(tunnel, value))
 
-    
-    elif choice_str== 'all_on':
-        return_str= (all_on(tunnel, value))
+    elif choice_str == 'all_on':
+        return_str = (all_on(tunnel, value))
 
     else:
-        return "No command \""+choice_str+"\" found. Please use help"
+        return "No command \"" + choice_str + "\" found. Please use help"
 
-    return(return_str)
+    return return_str
+
 
 def main():
-
     tunnel = sdl_knx.KNX_tunnel('192.168.1.99')
-    
+    sdl_knx.set_rgb(tunnel, toknx('14/3/1'), [0, 90, 100, 200])
+    time.sleep(1)
+    print(sdl_knx.get_rgb(tunnel, toknx('14/3/1')))
+
+    return True
+"""
+def main():
+    tunnel = sdl_knx.KNX_tunnel('192.168.1.99')
+    sdl_knx.set_rgb(tunnel, toknx('14/3/21'), [0, 0, 0, 200])
     while True:
-        choice_str=None
+        choice_str = None
         if not choice_str:
-            while not choice_str: 
+            while not choice_str:
                 choice_str = input("Witch demo would you like to run? (help for more infos) \n")
-                
+
         if choice_str == 'exit':
             while True:
-                choice_str= input("Exiting the app. Are you sure (y/n)? ")
-                if choice_str in ['y','Y','yes','Yes']: 
+                choice_str = input("Exiting the app. Are you sure (y/n)? ")
+                if choice_str in ['y', 'Y', 'yes', 'Yes']:
                     return 0
-                elif choice_str in ['n','N','no','No']:
+                elif choice_str in ['n', 'N', 'no', 'No']:
                     break
         else:
-            split_str = choice_str.split(' ',1)
-            if len(split_str) ==2:
+            split_str = choice_str.split(' ', 1)
+            if len(split_str) == 2:
                 print(multiple_choice(split_str[0], tunnel, split_str[1]))
-            elif len(split_str) ==1:
+            elif len(split_str) == 1:
                 print(multiple_choice(split_str[0], tunnel))
             time.sleep(2)
 
     time.sleep(2)
 
-"""   
+
+
     sdl_knx.set_rgb(tunnel, toknx('14/3/21'), [0, 0, 0, 50])
     time.sleep(1)
     print(sdl_knx.get_rgb(tunnel, toknx('14/3/21')))
     sdl_knx.setrgb(tunnel, toknx('14/3/21'), [123, 200, 50, 20])
     time.sleep(1)
     print(sdl_knx.getrgb(tunnel, toknx('14/3/21')))
-
     sdl_knx.setled(tunnel, toknx(constants.WIKIHOUSE_1), 255)
     time.sleep(1)
     print(sdl_knx.getled(tunnel, toknx(constants.WIKIHOUSE_1)))
-
     time.sleep(1)
     #sdl_knx.alloff(tunnel)
     time.sleep(3)  # 3 secs seems to be the min to be sure setall is working properly...
     #sdl_knx.setallled(tunnel, 200)
     time.sleep(3)
     #sdl_knx.setallrgb(tunnel, [0, 0, 0, 200])
-
     sdl_knx.setled(tunnel, toknx(constants.WIKIHOUSE_1), 0)
     time.sleep(1)
     print(sdl_knx.getled(tunnel, toknx(constants.WIKIHOUSE_1)))
-
     sdl_knx.setled(tunnel, toknx(constants.WIKIHOUSE_2), 0)
     time.sleep(1)
     print(sdl_knx.getled(tunnel, toknx(constants.WIKIHOUSE_2)))
 """
-
-
-
-
 
 if __name__ == "__main__":
     main()
--- src/knx_bus_SDL.py
+++ src/knx_bus_SDL.py
@@ -4,16 +4,27 @@ from knxip.core import parse_group_address as toknx
 import time
 import src.constants as constants
 
-
 # All SDL constants
 rgb_first = toknx(constants.RGB_FIRST_KNX)
 
 led_first = toknx(constants.LED_FIRST_KNX)
 
+save_var_rgb = [[0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]]
+
 
 def KNX_tunnel(knx_gw_ip):
     """Open a tunnel with the KNX ethernet/knx bus module"""
-    
+
     tunnel = KNXIPTunnel(knx_gw_ip)
     if tunnel.connect():
         print('Tunnel opened')
@@ -21,20 +32,23 @@ def KNX_tunnel(knx_gw_ip):
     else:
         return None
 
+
 # All Led related fonction (Get, Set, Set All)
+
 
 def get_led(tunnel, led_id):
     """Fonction that return the led value (0-255) where address for "%status" is "%%" +2"""
-    
+
     try:
-        return list(bytearray(tunnel.group_read(led_id+2)))[0]
+        return list(bytearray(tunnel.group_read(led_id + 2)))[0]
     except:
         print("Unable to read from the KNX bus")
         return None
 
-def setled(tunnel, led_id, w_value=None):
+
+def set_led(tunnel, led_id, w_value=None):
     """Fonction that set the led value (0-255) for address for "%%" """
-    
+
     if not w_value:
         w_value = 0
     try:
@@ -43,9 +57,10 @@ def setled(tunnel, led_id, w_value=None):
     except:
         print("Unable to write to the KNX bus")
 
+
 def set_all_led(tunnel, w_value=None):
     """Set all LED lights to a common [White] value. Default: all off"""
-    
+
     if not w_value:
         w_value = 0
 
@@ -58,28 +73,29 @@ def set_all_led(tunnel, w_value=None):
 def get_rgb(tunnel, rgb_id):
     """Fonction that return the RGBW value (0-255) in an array, 
     where address for "%status" is "%%" +2"""
-    
+
     rgbw_value = [0, 0, 0, 0]
     try:
-        rgbw_value[0] = list(bytearray(tunnel.group_read(rgb_id + 2)))[0]
-        rgbw_value[1] = list(bytearray(tunnel.group_read(rgb_id + 7)))[0]
-        rgbw_value[2] = list(bytearray(tunnel.group_read(rgb_id + 12)))[0]
-        rgbw_value[3] = list(bytearray(tunnel.group_read(rgb_id + 17)))[0]
+        rgbw_value[0] = list(bytearray(tunnel.group_read(rgb_id + 4)))[0]
+        rgbw_value[1] = list(bytearray(tunnel.group_read(rgb_id + 9)))[0]
+        rgbw_value[2] = list(bytearray(tunnel.group_read(rgb_id + 14)))[0]
+        rgbw_value[3] = list(bytearray(tunnel.group_read(rgb_id + 19)))[0]
     except:
         print("Unable to read from the KNX bus")
         return None
     return rgbw_value
 
+
 def set_rgb(tunnel, rgb_id, rgbw_value=None):
     """Fonction that set the RGBW value (0-255) array, for address for "%%" """
-    
+
     if not rgbw_value:
         rgbw_value = [0, 0, 0, 0]
     try:
-        tunnel.group_write(rgb_id + 0,  [0])
-        tunnel.group_write(rgb_id + 2,  [rgbw_value[0]])
-        tunnel.group_write(rgb_id + 5,  [0])
-        tunnel.group_write(rgb_id + 7,  [rgbw_value[1]])
+        tunnel.group_write(rgb_id + 0, [0])
+        tunnel.group_write(rgb_id + 2, [rgbw_value[0]])
+        tunnel.group_write(rgb_id + 5, [0])
+        tunnel.group_write(rgb_id + 7, [rgbw_value[1]])
         tunnel.group_write(rgb_id + 10, [0])
         tunnel.group_write(rgb_id + 12, [rgbw_value[2]])
         tunnel.group_write(rgb_id + 15, [0])
@@ -90,7 +106,7 @@ def set_rgb(tunnel, rgb_id, rgbw_value=None):
 
 def set_all_rgb(tunnel, rgbw_value=None):
     """Set all RGB lights to a common [RGBW] value. Default: all off"""
-    
+
     if not rgbw_value:
         rgbw_value = [0, 0, 0, 0]
 
@@ -100,13 +116,28 @@ def set_all_rgb(tunnel, rgbw_value=None):
 
 def set_all_rgb_random(tunnel):
     """ Set all RGB lights to a random and different RGB value """
-    
-    for light_it in range(0, constants.RGB_TOTAL + 1):
-        set_rgb(tunnel, rgb_first + (constants.RGB_STEP * light_it), [randint(0, 255), randint(0, 255), randint(0, 255), 0])
+
+    for light_it in range(0, constants.RGB_TOTAL):
+        set_rgb(tunnel, rgb_first + (constants.RGB_STEP * light_it),
+                [randint(0, 255), randint(0, 255), randint(0, 255), 0])
     return True
 
 
-def disco_RGB_mode(tunnel):
+def save_rgb_all(tunnel):
+
+    for light_it in range(0, constants.RGB_TOTAL):
+        save_var_rgb[light_it] = get_rgb(tunnel, rgb_first + (constants.RGB_STEP * light_it))
+        print(save_var_rgb[light_it])
+        time.sleep(3)
+
+
+def restore_rgb_all(tunnel):
+    for light_it in range(0, constants.RGB_TOTAL):
+        set_rgb(tunnel, rgb_first + (constants.RGB_STEP * light_it), save_var_rgb[light_it])
+        print(save_var_rgb[light_it])
+
+
+def disco_rgb_mode(tunnel):
     while True:
         set_all_rgb_random(tunnel)
         time.sleep(3)

d3961e63628ef1c07278a34b1be79e5322b73f7d
Faraut Victor, ENT-DES-DTO <victor.faraut@gmail.com>
2017-04-26 16:30:17 +0200

    Reconnection with disconnection and delay to try if it is more stable that before

--- src/knx_bus_SDL.py
+++ src/knx_bus_SDL.py
@@ -184,6 +184,8 @@ def set_led(tunnel, led_id, w_value=None):
         if tunnel.check_connection_state():
             print("Unable to write to the KNX bus")
         else:
+            tunnel.disconnect()
+            time.sleep(3)
             tunnel.connect()
         return 1
 
@@ -235,6 +237,8 @@ def set_rgb(tunnel, rgb_id, rgbw_value=None):
         if tunnel.check_connection_state():
             print("Unable to write to the KNX bus")
         else:
+            tunnel.disconnect()
+            time.sleep(3)
             tunnel.connect()
         return 1
 

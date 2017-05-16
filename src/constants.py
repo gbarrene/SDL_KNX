4dcf6190ecd0fc1acceec28a7c587c3a9377c962
Faraut Victor, ENT-DES-DTO <victor.faraut@gmail.com>
2017-05-16 08:53:13 +0200

    Update constant not right

--- src/constants.py
+++ src/constants.py
@@ -181,11 +181,11 @@ LED_POSITION = [[0, 0, 'SQUAD_2_A'],
 #  DevEUI,  Zone_name,  light level 1, light level 2,  light level 3
 #
 #
-LORA_SENSOR = [['A81758FFFE03045F', 'PRESENTATION', 200, 180],
-               ['A81758FFFE030460', 'Acceuil', 200, 180],
-               ['A81758FFFE030461', 'Squad_3', 230, 200],
+LORA_SENSOR = [['A81758FFFE03045F', 'Squad_3', 200, 180],
+               ['A81758FFFE030460', 'Squad_1, 200, 180],
+               ['A81758FFFE030461', 'PRESENTATION', 230, 200],
                ['A81758FFFE030425', 'Squad_2', 200, 180],
-               ['A81758FFFE0303FC', 'Squad_1', 200, 180],
+               ['A81758FFFE0303FC', 'Acceuil', 200, 180],
                ['A81758FFFE0301CC', 'WIKIHOUSE_1', 200, 180],
                ['A81758FFFE0304B5', 'WIKIHOUSE_2', 200, 180],
                ['A81758FFFE0304B7', 'WIKIHOUSE_3', 200, 180]

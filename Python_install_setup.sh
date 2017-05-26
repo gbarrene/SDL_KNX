e361192f6e01089420fcb0efdd476625ffd2e656
Faraut Victor, ENT-DES-DTO <victor.faraut@gmail.com>
2017-05-26 17:08:08 +0200

    Auto setup update

--- Python_install_setup.sh
+++ Python_install_setup.sh
@@ -2,8 +2,8 @@
 # Setup a new raspberry pi jessie (not lite) for using the KNX and node-red
 # 
 
-sudo apt-get update -Y
-sudo apt-get upgrade -Y
+sudo apt-get update
+yes Y | sudo apt-get upgrade
 
 
 sudo apt-get install python3-pip
@@ -20,13 +20,18 @@ sudo pip3 install knxip
 #sudo apt-get install git
 #cd ~/Documents
 #sudo git clone https://github.com/gbarrene/SDL_KNX.git
-
-sudo apt-get install npm -Y
+yes Y | sudo apt-get install npm
 sudo npm install -g npm@2.x 
+node-red-start
+^C
+node-red-stop
 hash -r
 cd /home/pi/.node-red
 sudo npm install node-red-dashboard
 sudo systemctl enable nodered.service
+node-red-start
+
+sudo python3 Light_flask_api.py
 
 exit
 

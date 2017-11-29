e9d35ce586fa7802c0570b47d82bff57fcb8b66d
lggoch <lucas.gauchoux@gmail.com>
2017-11-29 13:02:36 +0100

    remove overwrite

--- Light_flask_api.py
+++ Light_flask_api.py
@@ -262,28 +262,40 @@ def flic_presentation_click():
 
 @app.route("/flic_test", methods=['POST'])
 def flic_test():
-    addresses = [['14/2/41', 1, 0],
-                 ['14/2/46', 1, 0],
-                 ['14/3/81', 4, 0],
-                 ['14/3/101', 4, 0],
-                 ['14/3/121', 4, 190],
-                 ['14/3/141', 4, 190],
-                 ['14/3/161', 4, 230],
-                 ['14/3/181', 4, 230],
-                 ['14/3/201', 4, 230],
-                 ['14/3/221', 4,230]]
-    for add in addresses:
-        if add[1] == 1:
-            if sdl_knx.set_led(tunnel, toknx(add[0]), add[2]):
-                print('1')
-                return 1
+    light_info_deveui = file_WR.RW_light_info_read()
+    sensor_id = 'A81758FFFE030461'
+    status = light_info_deveui[sensor_id]["flic_status"]
+    if status == 1:
+        addresses = [['14/2/41', 1, 0],
+                     ['14/2/46', 1, 0],
+                     ['14/3/81', 4, 0],
+                     ['14/3/101', 4, 0],
+                     ['14/3/121', 4, 190],
+                     ['14/3/141', 4, 190],
+                     ['14/3/161', 4, 230],
+                     ['14/3/181', 4, 230],
+                     ['14/3/201', 4, 230],
+                     ['14/3/221', 4,230]]
+        for add in addresses:
+            if add[1] == 1:
+                if sdl_knx.set_led(tunnel, toknx(add[0]), add[2]):
+                    print('1')
+                    return 1
+            else:
+                if sdl_knx.set_rgb(tunnel, toknx(add[0]),[0,0,0,add[2]]):
+                    print('1')
+                    return 1
+        file_WR.RW_light_info_update('presentation', 'flic_status', 0)
+        return "All lights"
+    else:
+        zone_name = "presentation"
+        color = [0, 0, 0, 253]
+        file_WR.RW_light_info_update('presentation', 'flic_status', 1)
+        if sdl_knx.set_light_zone(tunnel, zone_name, color):
+            restart()
+            return "Unable to write to the KNX bus"
         else:
-            if sdl_knx.set_rgb(tunnel, toknx(add[0]),[0,0,0,add[2]]):
-                print('1')
-                return 1
-    file_WR.RW_light_info_update('presentation', 'flic_status', 0)
-    return "All lights"
-
+            return "All lights were set successfully"
 
 
 @app.route('/flic_presentation/hold', methods=['POST'])
